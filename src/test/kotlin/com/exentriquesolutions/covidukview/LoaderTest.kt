59858f8f6f3649aa3cb2ae330adf9c8767c3bbb2
rses <rs@exentriquesolutions.com>
2020-10-11 15:51:00 +0100

    Optimise imports

--- src/test/kotlin/com/exentriquesolutions/covidukview/LoaderTest.kt
+++ src/test/kotlin/com/exentriquesolutions/covidukview/LoaderTest.kt
@@ -1,15 +1,10 @@
 package com.exentriquesolutions.covidukview
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.greaterThan
 import com.natpryce.hamkrest.hasSize
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 
 val isNotEmpty = hasSize(greaterThan(0))
 
